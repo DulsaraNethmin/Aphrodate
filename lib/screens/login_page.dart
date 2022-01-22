0aa772e0d3bda9cc28e04f0b53aa273b7cd7b288
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-01-22 12:28:34 +0530

    firebase setup done and login page added

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,97 @@
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Padding(
+      padding: EdgeInsets.all(20),
+      child: Hero(
+          tag: 'hero',
+          child: CircleAvatar(
+            backgroundColor: Colors.red.shade900,
+            radius: 56.0,
+            child: Image.asset('images/app-logo.png'),
+          )
+      ),
+    );
+    final loginText= Row(
+      children: [Text("Login",style: TextStyle(color: Colors.black,fontFamily: 'Comfortaa-Bold', fontSize: 36,letterSpacing: 2.0),)],
+    );
+    final inputEmail = Padding(
+      padding: EdgeInsets.only(bottom: 10,top: 10),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            hintText: 'Email',
+            contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50.0)
+            )
+        ),
+      ),
+    );
+    final inputPassword = Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        obscureText: true,
+        decoration: InputDecoration(
+            hintText: 'Password',
+            contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50.0)
+            )
+        ),
+      ),
+    );
+    final buttonLogin = Padding(
+      padding: EdgeInsets.only(bottom: 5),
+      child: ButtonTheme(
+        height: 56,
+        child: RaisedButton(
+          child: Text('Next', style: TextStyle(color: Colors.white, fontSize: 20,fontFamily: 'Roboto-mono')),
+          color: Colors.black87,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50)
+          ),
+          onPressed: () => {
+
+          },
+        ),
+      ),
+    );
+    final buttonForgotPassword = Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+    TextButton(
+    child: Text('Forgot Password', style: TextStyle(color: Colors.grey, fontSize: 16,fontFamily: 'Roboto-mono'),),
+        onPressed: null
+    ),
+      ],
+    );
+
+    return SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              children: <Widget>[
+                logo,
+                loginText,
+                inputEmail,
+                inputPassword,
+                buttonLogin,
+                buttonForgotPassword
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
