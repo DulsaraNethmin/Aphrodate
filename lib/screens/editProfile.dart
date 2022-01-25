da55d2b7c7f86ddaa2883502ba1d00692e316e27
Nethmin Dulsara <dulsaranethmin@gmail.com>
2022-01-25 17:24:37 +0530

    form added

--- lib/screens/editProfile.dart
+++ lib/screens/editProfile.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({Key? key}) : super(key: key);
@@ -9,6 +12,19 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  //final networkHandler = NetworkHandler();
+  bool circular = false;
+  late PickedFile _imageFile;
+  final _globalkey = GlobalKey<FormState>();
+  TextEditingController _name = TextEditingController();
+  TextEditingController _profession = TextEditingController();
+  TextEditingController _dob = TextEditingController();
+  TextEditingController _title = TextEditingController();
+  TextEditingController _about = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  File ? image;
+  String? _value;
+  String? _value_sex;
   @override
   Widget build(BuildContext context) {
     final topAppBar = AppBar(
@@ -24,10 +40,310 @@ class _EditProfileState extends State<EditProfile> {
       elevation: 0,
       backgroundColor: Colors.white,
     );
+    final saveButton=Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        IconButton(icon: Icon(Icons.save),onPressed: (){},),
+      ],
+    );
     return Scaffold(
       appBar: topAppBar,
-      body: SafeArea(
-        child: Text('edit profile'),
+      body: Form(
+        key: _globalkey,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+          children: <Widget>[
+            saveButton,
+            imageProfile(),
+            SizedBox(
+              height: 20,
+            ),
+            nameTextField(),
+            SizedBox(
+              height: 20,
+            ),
+            gender(),
+            SizedBox(
+              height: 20,
+            ),
+            sexuality(),
+            SizedBox(
+              height: 20,
+            ),
+            titleTextField(),
+            SizedBox(
+              height: 20,
+            ),
+            aboutTextField(),
+            SizedBox(
+              height: 20,
+            ),
+            InkWell(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget imageProfile() {
+    return Center(
+      child: Stack(children: <Widget>[
+        CircleAvatar(
+          radius: 80.0,
+          backgroundImage:(image==null)? AssetImage('images/man.png'):FileImage(image!) as ImageProvider,
+        ),
+        Positioned(
+          bottom: 20.0,
+          right: 20.0,
+          child: InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                builder: ((builder) => bottomSheet()),
+              );
+            },
+            child: Icon(
+              Icons.camera_alt,
+              color: Colors.black,
+              size: 28.0,
+            ),
+          ),
+        ),
+      ]),
+    );
+  }
+
+  Widget bottomSheet() {
+    return Container(
+      height: 100.0,
+      width: MediaQuery.of(context).size.width,
+      margin: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Choose Profile photo",
+            style: TextStyle(
+              fontSize: 20.0,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.camera),
+              onPressed: () {
+                takePhoto(ImageSource.camera);
+              },
+              label: Text("Camera"),
+            ),
+            FlatButton.icon(
+              icon: Icon(Icons.image),
+              onPressed: () {
+                takePhoto(ImageSource.gallery);
+              },
+              label: Text("Gallery"),
+            ),
+          ])
+        ],
+      ),
+    );
+  }
+
+  void takePhoto(ImageSource source) async {
+    final pickedFile = await ImagePicker().pickImage(
+      source: source,
+    );
+    setState(() {
+      this.image = File(pickedFile!.path);
+    });
+  }
+
+  Widget nameTextField() {
+    return TextFormField(
+      controller: _name,
+      validator: (value) {
+        if (value == null){ return "Name can't be empty";}
+
+        return null;
+      },
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+            )),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )),
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.green,
+        ),
+        labelText: "Name",
+        helperText: "Name can't be empty",
+        hintText: "Dev Stack",
+      ),
+    );
+  }
+
+  Widget gender() {
+    List<String> list_items=['Male','Female','Binary'];
+    return  Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 1),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child:DropdownButtonHideUnderline(
+        child: DropdownButton(
+          value: _value,
+          items: list_items.map((String item) {
+            return DropdownMenuItem<String>(
+              child: Text('$item'),
+              value: item,
+            );
+          }).toList(),
+          onChanged:(value) {
+            setState(() {
+              _value = value as String;
+            });
+          },
+          hint:Text("Gender"),
+          disabledHint:Text("Disabled"),
+          elevation: 8,
+          style:TextStyle(color:Colors.black, fontSize: 16),
+          icon: Icon(Icons.arrow_drop_down_circle),
+          iconDisabledColor: Colors.red,
+          iconEnabledColor: Colors.black,
+          isExpanded: true,
+        ),
+      ),
+    );
+  }
+
+
+  Widget sexuality() {
+    List<String> list_items=['Straight','Gay','Lesbian','Bisexual'];
+    return  Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 1),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child:DropdownButtonHideUnderline(
+        child: DropdownButton(
+          value: _value_sex,
+          items: list_items.map((String item) {
+            return DropdownMenuItem<String>(
+              child: Text('$item'),
+              value: item,
+            );
+          }).toList(),
+          onChanged:(value) {
+            setState(() {
+              _value_sex = value as String;
+            });
+          },
+          hint:Text("Sexuality"),
+          disabledHint:Text("Disabled"),
+          elevation: 8,
+          style:TextStyle(color:Colors.black, fontSize: 16),
+          icon: Icon(Icons.arrow_drop_down_circle),
+          iconDisabledColor: Colors.red,
+          iconEnabledColor: Colors.black,
+          isExpanded: true,
+        ),
+      ),
+    );
+  }
+
+
+
+
+  Widget dobField() {
+    return TextFormField(
+      controller: _dob,
+      validator: (value) {
+        if (value == null) return "DOB can't be empty";
+
+        return null;
+      },
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+            )),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )),
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.green,
+        ),
+        labelText: "Date Of Birth",
+        helperText: "Provide DOB on dd/mm/yyyy",
+        hintText: "01/01/2020",
+      ),
+    );
+  }
+
+  Widget titleTextField() {
+    return TextFormField(
+      controller: _title,
+      validator: (value) {
+        if (value == null) return "Title can't be empty";
+
+        return null;
+      },
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+            )),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )),
+        prefixIcon: Icon(
+          Icons.person,
+          color: Colors.green,
+        ),
+        labelText: "Title",
+        helperText: "It can't be empty",
+        hintText: "Flutter Developer",
+      ),
+    );
+  }
+
+  Widget aboutTextField() {
+    return TextFormField(
+      controller: _about,
+      validator: (value) {
+        if (value == null)
+          return "Bio can't be empty";
+
+        return null;
+      },
+      maxLines: 4,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+            )),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.orange,
+              width: 2,
+            )),
+        labelText: "Bio",
+        helperText: "Write about yourself",
+        hintText: "I am Jane Smith",
       ),
     );
   }
